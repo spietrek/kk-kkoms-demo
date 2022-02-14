49498cf74963ac6d7b1a370e2e4b0eb094171815
spietrek <spietrek@gmail.com>
2022-02-14 13:36:19 -0500

    add storybook-vue-router dependency

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
 import '@/main.css'
+import StoryRouter from 'storybook-vue-router'
+import { addDecorator } from '@storybook/vue'
+
+addDecorator(StoryRouter())
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
